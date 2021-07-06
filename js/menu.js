9d1c6d826bf2f2f5708beab00e52f147ff5df499
DachoSlav <david.312kocevski@outlook.com>
2021-07-06 09:51:41 +0200

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
     $(".starters").click(function(){
-        $(".starter").toggle();
+        $(".starter").toggle(0.5);
+                
     })
 
     $(".salads").click(function(){
         $(".salad").toggle();
+    })
+
+    $(".pizzas").click(function(){
+        $(".pizza").toggle();
+    })
+
+    $(".pastas").click(function(){
+        $(".pasta").toggle();
     })
 })
